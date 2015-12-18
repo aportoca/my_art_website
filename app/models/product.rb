2f8a5f412fbb97fa28debc6a3151a71bfc93c9ff
aportoca@gmail.com <aportoca@gmail.com>
2015-12-17 18:34:29 -0800

    layout stuff!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,14 @@
 class Product < ActiveRecord::Base
-  
+
+  default_scope { order('title') } 
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
+  validates :title, :length => { :minimum => 10, :message => 'must be at least ten characters long.'}
+
   
-  validates :image_url, :format => {
-    :with => %r{\.(gif|jpg|png)$}i,
-    :message => 'must be a URL for gif, jpg or png image'
-  }
+  #validates :image_url, :format => {
+  #  :with => %r{\.(gif|jpg|png)$}i,
+  #  :message => 'must be a URL for gif, jpg or png image'
+  #}
 end
